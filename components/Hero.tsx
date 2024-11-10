8bb507caa2dbe8a85ccaf2ed29a993ef2834faee
Osiris8 <miganosiris8@gmaaail.com>
2024-11-10 14:22:41 +0100

    add Hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+export default function Hero() {
+  return (
+    <div className="hero bg-base-100 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-6xl font-bold">Starting Build Anythings</h1>
+          <p className="py-6 text-xl">
+            Hi, I will happy to work with you. L&#39;ets build your next project
+            !
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}

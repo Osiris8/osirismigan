ef9a7c224105480aca761b1cec3930dddf798d89
Osiris8 <miganosiris8@gmaaail.com>
2025-09-27 00:04:22 +0200

    Update button again

--- components/Project.tsx
+++ components/Project.tsx
@@ -163,7 +163,7 @@ export default function Project() {
                 </div>
                 <div className="flex justify-between">
                   {project.codeLink && (
-                    <Button asChild className="bg-black m-1">
+                    <Button asChild variant="secondary" className="m-1">
                       <Link
                         href={project.codeLink}
                         target="_blank"
